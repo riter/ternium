67d1b9d7444a7e697dd5d6b1283d55164c0a6563
chooseone7 <marcjav7@gmail.com>
2014-10-29 17:34:25 -0430

    consultas2

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -2,171 +2,182 @@
 
 class DashboardController extends AppController {
 
-    public function beforeFilter() {   
+    /**
+     * Components
+     *
+     * @var array
+     */
+    public $components = array('Paginator');
+
+    public function beforeFilter() {
         parent::beforeFilter();
         // Controller spesific beforeFilter
-        
+
         $user = AuthComponent::user();
         $this->loadModel('User');
         $this->loadModel('Paise');
         $this->loadModel('Calculo');
         $this->loadModel('Provincia');
         $this->loadModel('Profesione');
-        
-        
+
+
         if (!AuthComponent::user()) {
-            $this->redirect(array('controller'=>'home', 'action'=>'login', null, 'admin'=>false));
-        }else{
-           if (AuthComponent::user('role') != User::_ADMIN){
-                $this->redirect(array('controller'=>'home', 'action'=>'login', null, 'admin'=>false));
-            } 
-        }            
-    }
-    
-    public function admin_index() 
-    {
-        $this->UsuariosPais();
-        echo "<br>";
-        $this->UsuariosProfesion();
-        echo "<br>";
-        $this->UsuariosEdad();
-        echo "<br/>";
-        $this->UsuariosProvincia();
-        echo "<br/>";
-        $this->listadosUsuarios('Usuarios registrados');
-       $this->set('title_page', 'Dashboard');
-      
+            $this->redirect(array('controller' => 'home', 'action' => 'login', null, 'admin' => false));
+        } else {
+            if (AuthComponent::user('role') != User::_ADMIN) {
+                $this->redirect(array('controller' => 'home', 'action' => 'login', null, 'admin' => false));
+            }
+        }
     }
 
-       public function UsuariosEdad()
-       {
-           $fields=array('User.uid','User.contar_usuario');
-           $contarUs = $this->User->find('all',array(
-          'conditions'=>array('User.uid >'=>0),//,array('User.fecha_nacimiento'=>'2001-08-09')),
-          'recursive'=>0,
-          'group'=>'User.nombre',
-          'limit'=>10,
-          'recursive'=>0,$fields));
-           
-           
-        $edad=0;
-         foreach ($contarUs as $contar){
-                
-             $fechaaa= $contar['User']['fecha_nacimiento'];
-              $edad= $this->calcular_edad($fechaaa);
-              if($edad>=10 && $edad<=25)
-                  echo $contar['User']['edad_usuario']. " usuarios 23/25 a&ntilde;os <br/>";
-                  elseif ($edad>=26 && $edad<=29)
-                       echo $contar['User']['edad_usuario']. " usuarios 26/29 a&ntilde;os <br/>";
-                  elseif ($edad>=30 && $edad<=35) 
-                       echo $contar['User']['edad_usuario']. " usuarios 30/35 a&ntilde;os <br/>";
-                  elseif ($edad>=36 && $edad<=42) 
-                       echo $contar['User']['edad_usuario']. " usuarios 36/42 a&ntilde;os <br/>";
-              }
-                            
-       }
-      public function listadosUsuarios($listado){
-          switch ($listado){
-              case "Usuarios registrados";
-                  $cont = $this->User->find('count',array('conditions'=>array('User.uid >'=>0)));
-                  echo "Usuarios registrados ".$cont;
-                   break;
-              case "Calculos realizados";
-                     $cont = $this->Calculo->find('count',array('conditions'=>array('Calculo.id >'=>0)));
-                     echo "Calculos realizados ".$cont;
-                   break;
-              case "Descargas de la apps";
-                  echo "en veremos!";
-                  break;
-              case "Descargas pdf";
-                  break;
-          
-          }
-          
-      }
-      public function calcular_edad($fechanacimiento){
-       list($ano,$mes,$dia) = explode("-",$fechanacimiento);
-        $ano_diferencia  = date("Y") - $ano;
+    public function admin_index() {
+        $up = $this->UsuariosPais();
+        // print_r($up);exit;
+        $upa = $this->UsuariosPaisAnio();
+        $ue = $this->UsuariosEdad();
+        $uprof = $this->UsuariosProfesion();
+        $prov = $this->UsuariosProvincia();
+
+        $recibir = array($up, $upa, $ue, $uprof, $prov);
+        $this->set('recibir', $recibir);
+    }
+
+    public function listadosUsuarios($listado) {
+        $listados[] = array();
+        switch ($listado) {
+            case "Usuarios registrados";
+                $listados = $this->User->find('count', array('conditions' => array('User.uid >' => 0)));
+                return $listados;
+                break;
+            case "Calculos realizados";
+                $cont = $this->Calculo->find('count', array('conditions' => array('Calculo.id >' => 0)));
+                echo "Calculos realizados " . $cont;
+                break;
+            case "Descargas de la apps";
+                echo "en veremos!";
+                break;
+            case "Descargas pdf";
+                break;
+        }
+    }
+
+    public function calcular_edad($fechanacimiento) {
+        list($ano, $mes, $dia) = explode("-", $fechanacimiento);
+        $ano_diferencia = date("Y") - $ano;
         $mes_diferencia = date("m") - $mes;
-        $dia_diferencia   = date("d") - $dia;
+        $dia_diferencia = date("d") - $dia;
         if ($dia_diferencia < 0 || $mes_diferencia < 0)
             $ano_diferencia--;
         return $ano_diferencia;
-}
-    
-          
-      public function UsuariosPais(){
-      $fields=array('User.uid','User.pais_id');
-      $contarUs = $this->User->find('all',array(
-          'conditions'=>array('User.uid >'=>0,array('User.fecha_nacimiento BETWEEN ? AND ?'=> array('2000-10-19','2014-10-27'))),
-          'group'=>'User.pais_id',
-          'recursive'=>0,$fields));
-      
-      foreach ($contarUs as $contar){
-        echo " <td>". $contar['User']['contar_usuario']."</td>";
-        echo " <td>". $contar['Paise']['nombre']."</td>";
-        echo " <td>". $contar['User']['fecha_nacimiento']."</td><br>";
-          
-        }
-      }
-      public function UsuariosPaisAnio(){
-      $fields=array('User.uid','User.pais_id');
-      $contarUs = $this->User->find('all',array(
-          'conditions'=>array('User.uid >'=>0,'User.created'=>'2014-10-02 09:38:20'),
-          'group'=>'User.pais_id',
-          'recursive'=>0,$fields));
-      
-      foreach ($contarUs as $contar){
-        echo " <td>". $contar['User']['contar_usuario']."</td>";
-        echo " <td>". $contar['Paise']['nombre']."</td>";
-        echo " <td>". $contar['User']['fecha_nacimiento']."</td><br>";
-          
-        }
-      }
-       public function UsuariosProfesion(){
-        
-           
-      $fields=array('User.uid','User.profesion_id');
-      $contarUs = $this->User->find('all',array(
-          'conditions'=>array('User.uid >'=>0,array('User.fecha_nacimiento BETWEEN ? AND ?'=> array('2000-10-19','2014-10-27'))),
-          'group'=>'User.profesion_id',
-          'recursive'=>0,$fields));
-      
-      foreach ($contarUs as $contar){
-        echo " <td>". $contar['User']['contar_usuario']."</td> ";
-         echo "<td>". $contar['Profesione']['nombre']."</td><br/>";
-          
-        }
-      }
-       public function UsuariosProvincia(){
-        
-           
-      $fields=array('User.uid','User.provincia_id');
-      $contarUs = $this->User->find('all',array(
-          'conditions'=>array('User.uid >'=>0,),
-          'group'=>'User.provincia_id',
-          'recursive'=>0,$fields));
-      
-      foreach ($contarUs as $contar){
-        echo " <td>". $contar['User']['contar_usuario']."</td> ";
-        echo "<td>". $contar['Provincia']['nombre']."</td><br/>";
-        
-        
-          
-        }
-      }
-      
-      
+    }
 
- 
+    public function UsuariosPais() {
+        $cargar[] = array();
+        $fields = array('User.uid', 'User.pais_id');
+        $contarUpaiss = $this->User->find('all', array(
+            'conditions' => array('User.uid >' => 0, array('User.fecha_nacimiento BETWEEN ? AND ?' => array('2000-10-19', '2014-10-27'))),
+            'group' => 'User.pais_id',
+            'recursive' => 0));
+        $i = 0;
+        // echo "<pre>"; var_dump($contarUpaiss);echo "</pre>";exit;
+        foreach ($contarUpaiss as $contar) {
+            $cargar[$i][0] = $contar['User']['contar_usuario']." ";
+            $cargar[$i][1] = $contar['User']['nombre']. " ";
+            $cargar[$i][2] = $contar['User']['fecha_nacimiento']." <br/>";
+            $i++;
+        }
 
-      
-      
+        return $cargar;
+    }
+
+    public function UsuariosEdad() {
+        $cargar[] = array();
+        $fields = array('User.uid', 'User.contar_usuario');
+        $contarEdad = $this->User->find('all', array(
+            'conditions' => array('User.uid >' => 0, array('User.fecha_nacimiento BETWEEN ? AND ?' => array('2000-10-19', '2014-10-27'))),
+            'limit' => 10, //'group' => 'User.fecha_nacimiento',
+            'recursive' => 0, $fields)); //,'fields'=>$fields
+        $edad = 0;
+        $i = 0;
+        foreach ($contarEdad as $contar) {
+
+            $fechaaa = $contar['User']['fecha_nacimiento'];
+            $edad = $this->calcular_edad($fechaaa);
+
+            if ($edad <= 25) {// && $edad<26)
+                $cargar[$i][0] = $contar['User']['contar_usuario'] . " usuarios 23/25 a&ntilde;os <br/>";
+            } elseif ($edad <= 29) {
+                $cargar[$i][0] = $contar['User']['edad_usuario'] . " usuarios 26/29 a&ntilde;os <br/>";
+            } elseif ($edad <= 35) {
+                $cargar[$i][0] = $contar['User']['edad_usuario'] . " usuarios 30/35 a&ntilde;os <br/>";
+            } elseif ($edad <= 42) {
+                $cargar[$i][0] = $contar['User']['edad_usuario'] . " usuarios 36/42 a&ntilde;os <br/>";
+            }
+            $i++;
+        }
+        return $cargar;
+    }
+
+    public function UsuariosPaisAnio() {
+
+        $cargar[] = array();
+        $fields = array('User.uid', 'User.pais_id');
+        $contarUpanio = $this->User->find('all', array(
+            'conditions' => array('User.uid >' => 0, 'User.created <' => '2014-10-02 09:38:20'),
+            'group' => 'User.pais_id',
+            'recursive' => 0)); //,'fields'=>$fields
+
+        $i = 0;
+        foreach ($contarUpanio as $contar) {
+            $cargar[$i][0] = $contar['User']['contar_usuario']. " ";
+            $cargar[$i][1] = $contar['User']['nombre']. " ";
+            $cargar[$i][2] = $contar['User']['created']." <br/>";
+            $i++;
+        }
+        return $cargar;
+    }
+
+    public function UsuariosProfesion() {
+        $cargar[] = array();
+
+        $fields = array('User.uid', 'User.profesion_id');
+        $contarUs = $this->User->find('all', array(
+            'conditions' => array('User.uid >' => 0, array('User.fecha_nacimiento BETWEEN ? AND ?' => array('2000-10-19', '2014-10-27'))),
+            'group' => 'User.profesion_id',
+            'recursive' => 0)); //,'fields'=>$fields
+
+        $i = 0;
+        foreach ($contarUs as $contar) {
+            $cargar[$i][0] = $contar['User']['contar_usuario'] . " ";
+            $cargar[$i][1] = $contar['Profesione']['nombre']."<br/>";
+        }
+        return $cargar;
+    }
+
+    public function UsuariosProvincia() {
+        $cargar[] = array();
+
+        $fields = array('User.uid', 'User.provincia_id');
+        $contarUs = $this->User->find('all', array(
+            'conditions' => array('User.uid >' => 0), // array('User.fecha_nacimiento BETWEEN ? AND ?' => array('2000-10-19', '2014-10-27'))),
+            'group' => 'User.provincia_id',
+            'recursive' => 0)); //,'fields'=>$fields
+        $i = 0;
+
+        foreach ($contarUs as $contar) {
+            $cargar[$i][0] = $contar['User']['contar_usuario'] . " ";
+            $cargar[$i][1] = $contar['Provincia']['nombre']."<br/>";
+            $i++;
+        }//  echo "<br/>";
+        return $cargar;
+    }
+
     public function admin_home() {
-        $this->redirect(array('controller'=>'home', 'action'=>'login', null, 'admin'=>false));
-   }
-   public function consulta()
-   {
-      
-   }
+        $this->redirect(array('controller' => 'home', 'action' => 'login', null, 'admin' => false));
+    }
+
+    public function consulta() {
+        
+    }
+
 }
--- app/View/Dashboard/admin_index.ctp
+++ app/View/Dashboard/admin_index.ctp
@@ -1,3 +1,4 @@
+<?php/* echo print_r($recibir);*/?>
 <div class="mws-panel grid_8">
     
     
@@ -5,10 +6,27 @@
         <br><br><br><br><br><br><br>
          <img src="<?php echo $this->webroot; ?>/img/logo-steelframe.png" alt="Steelframe Calculator" title="Steelframe Calculator" />
          <?php
-         foreach($usuarios as $user)
-         {
-             echo $user['User']['nombre'];
-         }
+       
+        $contar_array=sizeof($recibir);
+        $contar_array2=count($recibir);
+        
+         $i=0;
+        
+         for($j=0;$j<count($recibir);$j++)
+        {   echo "<br/>";
+            for($i=0;$i<count($recibir[$j]);$i++){
+             
+               for($k=0;$k<count($recibir[$j][$i]);$k++){
+               echo $recibir[$j][$i][$k];
+               
+               }
+               
+            }
+           // echo "<pre>".$recibir[$j][1]."</pre>";
+           // echo "<pre>".$recibir[$j][2]."</pre>";
+           // echo "<pre>".$recibir[$j][3]."</pre>";
+           // echo "aaaaaa";exit;
+        }
          ?>
     </div>
     
